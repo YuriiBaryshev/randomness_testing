47034a27777d71c6eb6c04b9d648af84524ba86b
YuriiBaryshev <yura.baryshev@gmail.com>
2023-05-22 14:47:16 +0300

    fix passing condition

--- lib/src/randomness_testing_base.dart
+++ lib/src/randomness_testing_base.dart
@@ -5,7 +5,7 @@ import 'package:grizzly_distuv/math.dart';
 /// Checks if you are awesome. Spoiler: you are.
 class RandomnessTester {
   ///Implements monobit test
-  bool monobitTest(Uint8List data) {
+  static bool monobitTest(Uint8List data) {
     int s = 0;
     for(int i = 0; i < data.length; i++) {
       for (int mask = 0x80; mask != 0; mask = mask >> 1) {
@@ -21,6 +21,6 @@ class RandomnessTester {
 
     double pValue = erfc(sObs/sqrt(2));
 
-    return pValue < 0.01;
+    return pValue >= 0.01;
   }
 }
