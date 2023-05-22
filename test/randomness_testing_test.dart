117a82ce038adf8936ffc51bcefd383a273b53cf
YuriiBaryshev <yura.baryshev@gmail.com>
2023-05-22 14:47:53 +0300

    add failing test cases

--- test/randomness_testing_test.dart
+++ test/randomness_testing_test.dart
@@ -1,14 +1,17 @@
+import 'dart:typed_data';
+
 import 'package:randomness_testing/randomness_testing.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    setUp(() {
-      // Additional setup goes here.
-    });
+    Uint8List all3BytesOnes = Uint8List.fromList([0xff, 0xff, 0xff]);
+    Uint8List all3BytesZeros = Uint8List.fromList([0x0, 0x0, 0x0]);
+    Uint8List all3BytesA = Uint8List.fromList([0xaa, 0xaa, 0xaa]);
 
-    test('First Test', () {
-
+    test('fails for all the same bits', () {
+      expect(RandomnessTester.monobitTest(all3BytesZeros), isFalse);
+      expect(RandomnessTester.monobitTest(all3BytesOnes), isFalse);
     });
   });
 }
