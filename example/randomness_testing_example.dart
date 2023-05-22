daeefaab3a6f767cb0961a5a609103bb3b6d7827
YuriiBaryshev <yura.baryshev@gmail.com>
2023-05-22 13:30:50 +0300

    cleared default project's artifacts

--- example/randomness_testing_example.dart
+++ example/randomness_testing_example.dart
@@ -1,6 +1,5 @@
 import 'package:randomness_testing/randomness_testing.dart';
 
 void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+
 }
--- lib/src/randomness_testing_base.dart
+++ lib/src/randomness_testing_base.dart
@@ -1,6 +1,7 @@
-// TODO: Put public facing types in this file.
-
 /// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+class RandomnessTester {
+  bool monobitTest() {
+    bool hasPassed = true;
+    return hasPassed;
+  }
 }
