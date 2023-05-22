2c34c31d3c74da846de92d6c908d145d146f36d5
YuriiBaryshev <yura.baryshev@gmail.com>
2023-05-22 13:03:11 +0300

    create project carcass

---
+++ example/randomness_testing_example.dart
@@ -0,0 +1,6 @@
+import 'package:randomness_testing/randomness_testing.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/randomness_testing.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library randomness_testing;
+
+export 'src/randomness_testing_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
