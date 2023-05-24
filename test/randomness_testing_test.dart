0acc52dfcc301b3c98d664801e3e2432a9a54a20
YuriiBaryshev <yura.baryshev@gmail.com>
2023-05-24 15:27:23 +0300

    adapt test cases for FIPS 140

--- test/randomness_testing_test.dart
+++ test/randomness_testing_test.dart
@@ -1,32 +1,41 @@
 import 'dart:typed_data';
+import 'dart:math' show Random;
 
 import 'package:randomness_testing/randomness_testing.dart';
 import 'package:test/test.dart';
 
 void main() {
   group('A group of tests', () {
-    Uint8List all3BytesOnes = Uint8List.fromList([0xff, 0xff, 0xff]);
-    Uint8List all3BytesZeros = Uint8List.fromList([0x0, 0x0, 0x0]);
+    int length = 2500;
+    Uint8List allBytesOnes = Uint8List(length);
+    Uint8List allBytesZeros = Uint8List(length);
 
-    Uint8List all3BytesA = Uint8List.fromList([0xaa, 0xaa, 0xaa]);
-    Uint8List set4Bytes = Uint8List.fromList([0x00, 0xff, 0x00, 0xff]);
+    Uint8List allBytesA = Uint8List(length);
+    Uint8List halfOnesHalfZeros = Uint8List(length);
+    Uint8List randomOutput = Uint8List(length);
 
-
-    test('fails for all the same bits', () {
-      expect(RandomnessTester.monobitTest(all3BytesZeros), isFalse);
-      expect(RandomnessTester.monobitTest(all3BytesOnes), isFalse);
+    setUp(() {
+      var rand = Random.secure();
+      for (int i = 0; i < length; i++) {
+        allBytesOnes[i] = 0xff;
+        allBytesZeros[i] = 0;
+        allBytesA[i] = 0xaa;
+        halfOnesHalfZeros[i] = (i < 1250) ? 0xff : 0;
+        randomOutput[i] = rand.nextInt(256);
+      }
     });
 
 
     test('fails for all the same bits', () {
-      expect(RandomnessTester.monobitTest(all3BytesA), isTrue);
-      expect(RandomnessTester.monobitTest(set4Bytes), isTrue);
+      expect(RandomnessTester.monobitTest(allBytesZeros), isFalse);
+      expect(RandomnessTester.monobitTest(allBytesOnes), isFalse);
     });
 
 
-    test('monobit test for binary string from NIST STS', () {
-      expect(RandomnessTester.monobitTestForBinaryString("1011010101"), isTrue);
-      expect(RandomnessTester.monobitTestForBinaryString("0100101010"), isTrue);
+    test('fails for all the same bits', () {
+      expect(RandomnessTester.monobitTest(allBytesA), isTrue);
+      expect(RandomnessTester.monobitTest(halfOnesHalfZeros), isTrue);
+      expect(RandomnessTester.monobitTest(randomOutput), isTrue);
     });
 
 
