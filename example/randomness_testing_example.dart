2441a47f609fbc3f2194a391ca0d7c231a3e661b
YuriiBaryshev <yura.baryshev@gmail.com>
2023-05-24 15:43:51 +0300

    enhance example

--- example/randomness_testing_example.dart
+++ example/randomness_testing_example.dart
@@ -1,5 +1,17 @@
+import 'dart:typed_data';
+import 'dart:math' show Random;
 import 'package:randomness_testing/randomness_testing.dart';
 
 void main() {
+  Uint8List randomOutput = Uint8List(2500);
 
+  var rand = Random.secure();
+  for (int i = 0; i < 2500; i++) {
+    randomOutput[i] = rand.nextInt(256);
+  }
+  if (RandomnessTester.monobitTest(randomOutput)) {
+   print("Generated sequence passed tests");
+  } else {
+    print("Generated sequence didn't pass tests");
+  }
 }
